420c1eb1a1ee9ece8016ccedefb11908a69b6973
zhanghao <zhanghao@zcool.com.cn>
2017-12-29 14:13:12 +0800

    add git ignore

--- app.js
+++ app.js
@@ -9,16 +9,19 @@ App({
     // 登录
     wx.login({
       success: res => {
+        console.log('----------------------------------- wx.login', res)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
     // 获取用户信息
     wx.getSetting({
       success: res => {
+        console.log('----------------------------------- wx.getSetting', res)
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
+              console.log('------------------------ getUserInfo', res)
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
 
